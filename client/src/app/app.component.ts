259a0324d32577198a7f771fce93cf4018aa849b
Hozepha <hozepha.abdoulalybarmal@gmail.com>
2025-02-10 18:12:27 +0100

    certificate installed

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { AppUser } from './AppUser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css', 
+  imports: [
+    FormsModule,
+    // NgFor
+]
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
-  title = 'Dating App';
-
-  public UserName!: string;
-
-  public UserId!: number;
+  title = 'Hozepha DA';
+  Users: any;
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe((item : AppUser | any) =>  { this.UserName = item.UserName, this.UserId = item.Id} );
+    this.http.get('https://localhost:5001/api/users')
+             .subscribe({
+              next: response => this.Users = response,
+              error: error => console.log(error),
+              complete: () => console.log("the request has completed")
+             });
   }
 }
